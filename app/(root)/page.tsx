797189d1398c5cfcde307534da40ac9dfae92d1e
mark <markferdinan.mfa@gmail.com>
2023-08-06 00:44:17 +0800

    added auth / page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
+//app/page.tsx
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
+    <div>
+      <h1 className="head-text text-left">Home</h1>
+    </div>
   );
 }
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RightSidebar() {
+  return <div></div>;
+}
+
+export default RightSidebar;
