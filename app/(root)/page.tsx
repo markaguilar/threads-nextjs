37ab3a3b24fba6f717e5b8f818c1a1e7b35dd4ba
mark <mark>
2023-08-26 19:13:07 +0800

    added community model , thread card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,32 @@
 import { currentUser } from "@clerk/nextjs";
-import { fetchThread } from "@/lib/actions/thread.actions";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
+import ThreadCard from "@/components/cards/ThreadCard";
+import Pagination from "@/components/shared/Pagination";
+
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
-  const result = await fetchThread(1, 30);
+  if (!user) return null;
 
-  console.log(result);
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    30,
+  );
+
   return (
-    <div>
+    <>
       <h1 className="head-text text-left">Home</h1>
+
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
           <p className="no-result">No threads found</p>
@@ -18,18 +36,26 @@ export default async function Home() {
               <ThreadCard
                 key={post._id}
                 id={post._id}
-                currentUserId={user?.id}
+                currentUserId={user.id}
                 parentId={post.parentId}
                 content={post.text}
                 author={post.author}
                 community={post.community}
                 createdAt={post.createdAt}
-                comments={post.comments}
+                comments={post.children}
               />
             ))}
           </>
         )}
       </section>
-    </div>
+
+      <Pagination
+        path="/"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
   );
 }
+
+export default Home;
