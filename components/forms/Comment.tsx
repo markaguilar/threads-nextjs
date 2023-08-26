4c68ec26e96f0e0eb6952a5a74a4718ed8e997cc
mark <mark>
2023-08-26 23:44:25 +0800

    added comment thread func

---
+++ components/forms/Comment.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { usePathname, useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { CommentValidation } from "@/lib/validations/thread";
+import { addCommentToThread, createThread } from "@/lib/actions/thread.actions";
+
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    await addCommentToThread(
+      threadId,
+      values.thread,
+      JSON.parse(currentUserId),
+      pathname,
+    );
+
+    form.reset();
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="comment-form">
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-3 w-full">
+              <FormLabel>
+                <Image
+                  src={currentUserImg}
+                  alt="Profile image"
+                  width={48}
+                  height={48}
+                  className="rounded-full object-cover"
+                />
+              </FormLabel>
+              <FormControl className="border-none bg-transparent">
+                <Input
+                  type="text"
+                  className="no-focus text-light-1 outline-none"
+                  placeholder="Comment ..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="comment-form_btn">
+          Reply
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default Comment;
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -4,13 +4,15 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs";
 
 import { sidebarLinks } from "@/constants";
 
 function LeftSidebar() {
   const router = useRouter();
   const pathname = usePathname();
+
+  const { userId } = useAuth();
 
   return (
     <section className="custom-scrollbar leftsidebar">
@@ -19,6 +21,8 @@ function LeftSidebar() {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
           return (
             <Link
               href={link.route}
