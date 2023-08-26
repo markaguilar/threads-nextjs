062629ce69043d3a63104a888dcd8bd448e7fca4
mark <mark>
2023-08-27 02:20:02 +0800

    added changes pages

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,22 +1,20 @@
-import React from "react";
 import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
-import { fetchUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
-
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import ThreadsTab from "@/components/shared/ThreadsTab";
 
 import { profileTabs } from "@/constants";
 
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
-
   if (!user) return null;
 
   const userInfo = await fetchUser(params.id);
-
   if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
@@ -43,12 +41,10 @@ async function Page({ params }: { params: { id: string } }) {
                   className="object-contain"
                 />
                 <p className="max-sm:hidden">{tab.label}</p>
+
                 {tab.label === "Threads" && (
-                  <p
-                    className="ml-1 rounded-sm bg-light-4 px-2 py-1
-                  !text-tiny-medium text-light-2"
-                  >
-                    {userInfo?.threads?.length}
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo.threads.length}
                   </p>
                 )}
               </TabsTrigger>
@@ -60,6 +56,7 @@ async function Page({ params }: { params: { id: string } }) {
               value={tab.value}
               className="w-full text-light-1"
             >
+              {/* @ts-ignore */}
               <ThreadsTab
                 currentUserId={user.id}
                 accountId={userInfo.id}
@@ -72,5 +69,4 @@ async function Page({ params }: { params: { id: string } }) {
     </section>
   );
 }
-
 export default Page;
